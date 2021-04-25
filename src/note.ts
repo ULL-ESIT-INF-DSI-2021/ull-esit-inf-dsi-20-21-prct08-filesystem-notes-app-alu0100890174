d0306a37fbc91ed442c611742a2358b665fc620b
Eduardo Pérez <alu0100890174@ull.edu.es>
2021-04-25 13:53:22 +0100

    Inicialización

---
+++ src/note.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import * as yargs from 'yargs';
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+      // Required logic to add a new note
+    }
+  },
+});
